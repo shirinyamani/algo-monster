e72ec8a442a80ea19501aa19b59a93e42ec825cd
shirin <yamani.shirin@ucalgary.ca>
2022-07-05 10:43:09 -0600

    deserialize added

--- dfs/leetcode/serialize_tree.py
+++ dfs/leetcode/serialize_tree.py
@@ -4,33 +4,35 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def serialize(root): #preorder traversal
-    res = [] 
-    def dfs(node):
-        if not node:
-            res.append("None") # serialize None as "None"
-            return 
+class CodeC:
+
+    def serialize(root): #preorder traversal
+        res = [] 
+        def dfs(node):
+            if not node:
+                res.append("None") # serialize None as "None"
+                return 
+            
+            res.append(str(node.val)) # serialize the node
+
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ", ".join(res)
+    
+    
+    def deserialize(self, data):
+        vals = data.split(",")
+        self.i = 0
         
-        res.append(str(node.val)) # serialize the node
-
-        dfs(node.left)
-        dfs(node.right)
-
-    dfs(root)
-    return ", ".join(res)
-    
-    
-def deserialize(data):
-    vals = data.split(",")
-    i = 0
-    
-    def dfs():
-        if vals[i] == "N":
-            i += 1
-            return None
-        node = TreeNode(int(vals[i]))
-        i += 1
-        node.left = dfs()
-        node.right = dfs()
-        return node
-    return dfs()
+        def dfs():
+            if vals[self.i] == "Null":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
