d1fb3bf2ef23a130e5dd975478a7dc689390c7b9
shirin <yamani.shirin@ucalgary.ca>
2022-07-29 13:56:00 -0600

    bs unknown size added

--- binary search/sorted array/binary_search_unknown_size.py
+++ binary search/sorted array/binary_search_unknown_size.py
@@ -7,7 +7,27 @@ def search_unknown_size(reader, target):
         if reader.get(mid) == target:
             return mid
         elif reader.get(mid) < target:
-            left = mid + 1
+            left = mid + 1 
         else:
             right = mid - 1
     return -1
+
+
+def search_unknown_size(arr, target):
+    i = 1
+    while ArrayReader.get(i) < target:
+        i = i * 2
+    return binary_search(arr, target, i // 2, i)
+
+def binary_search(arr, target, left, right):
+    while left <= right:
+        mid = (left + right) // 2
+        mid = ArrayReader.get(mid)
+
+        if arr[mid] <= target:
+            left = mid + 1
+
+        elif arr[mid] > target:
+            right = mid - 1
+
+    else: return -1
