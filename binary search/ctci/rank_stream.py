c749529df47d29f3d59e25a7fc8989376c65186d
shirin <yamani.shirin@ucalgary.ca>
2022-08-16 13:05:06 -0600

    ctci rank added

--- binary search/ctci/rank_stream.py
+++ binary search/ctci/rank_stream.py
@@ -5,17 +5,34 @@ class Node:
         self.right = None
         self.leftSize = 0
     
-    def insert(root, val):
-        if root is None:
-            return Node(val)
+        def insert(root, val): # O(log n) # BC insertion in arr is O(n)
+            if root is None:
+                return Node(val)
 
-        elif val < root.val:
-                root.left = insert(root.left, val)
-                root.leftSize += 1
-        else:
-            root.right = insert(root.right, val)
+            elif val < root.val:
+                    root.left = insert(root.left, val)
+                    root.leftSize += 1
+            else:
+                root.right = insert(root.right, val)
 
-        return root
+            return root
+
+        def get_rank(root, x):
+            if root.val == x:
+                return root.leftSize
+
+            elif x < root.val:
+                if root.left is None:
+                    return -1
+                else:
+                    return get_rank(root.left, x)
+            else:
+                if root.right is None:
+                    return -1
+                else:
+                    return root.leftSize + 1 + get_rank(root.right, x)
+        
+
 
 
 
