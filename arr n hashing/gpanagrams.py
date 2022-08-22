69b75edd94b69593effd45c4fda30cc9a79b2067
shirin <yamani.shirin@ucalgary.ca>
2022-08-22 11:32:50 -0600

    gp anagram added

--- arr n hashing/gpanagrams.py
+++ arr n hashing/gpanagrams.py
@@ -9,12 +9,12 @@ class Solution(object):
         anagram = collections.defaultdict(list) #{key: [value]}
         
         for s in strs:
-            res = ''.join(sorted(s))
+            res = ''.join(sorted(s)) # sort the string for each word for searching the char
             if res in anagram:
-                anagram[res].append(s)
+                anagram[res].append(s) # if the sorted string is already in the dictionary, append the word to the list
                 
             else:
-                anagram[res] = [s]
+                anagram[res] = [s] # if the sorted string is not in the dictionary, create a new key and value pair
                 
         return anagram.values()
                 
