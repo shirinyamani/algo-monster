b37c261a6485d04e0ea8f8397146fe89bed4c695
shirin <yamani.shirin@ucalgary.ca>
2022-08-25 10:48:19 -0600

    feature sort popularity added

--- arr n hashing/feature_sort_popularity.py
+++ arr n hashing/feature_sort_popularity.py
@@ -1,13 +1,21 @@
 import collections
 
-def sort_by_popularity(features, responses): 
-    
-    features_set = set(features)
-    order = {word: i for i, word in enumerate(features)}
-    freq = collections.defaultdict(int) # {word: freq}
+def sort_features(features, responses):
+    # freq ---> default dict (list)   features --- responses
+    # response split 
+    # order ---> i , word (enumerate)
+    feature_set = set(features)
+    freq = collections.defaultdict(int) #{feature:[num]}
+    order =  {word: i for i, word in enumerate(features)}
     for r in responses:
-        for word in set(r.split(' ')):
-            if word in features_set:
-                freq[word] += 1
-    features.sort(key=lambda x: (-freq[x], order[x]))
+        for f in set(r.split(' ')): # bc if repeated word, it will be counted twice
+            if f in feature_set:
+                freq[f] += 1  # {feature:num} for the sort purpose
+                
+    features.sort(key= lambda x: (-freq[x], order[x])) # - freq[x] for descending order
     return features
+
+
+
+if __name__ == "__main__":
+    print(sort_features(["easy", "touch"], ["i love the touch pad touch", "touch it was not easy to use"]))
